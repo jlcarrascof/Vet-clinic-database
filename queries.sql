e6f9630e0c1149b59ab833b90eb409c55909fb56
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-09-21 10:10:56 -0400

    Fix queries.sql according reviewer's suggestions

--- queries.sql
+++ queries.sql
@@ -39,7 +39,7 @@ BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
 SAVEPOINT mysavepoint;
 UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK TO my_savepoint;
+ROLLBACK TO mysavepoint;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 SELECT * FROM animals;
