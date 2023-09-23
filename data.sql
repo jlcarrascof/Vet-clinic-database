653dcf1e6e8f3b5fa6b1781b1e965d522ad80128
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-09-23 11:05:31 -0400

    Insert data to vets table

--- data.sql
+++ data.sql
@@ -41,4 +41,11 @@ UPDATE animals SET owner_id = (CASE
     WHEN name IN ('Charmander', 'Squirtle', 'Blossom') THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
     WHEN name IN ('Angemon', 'Boarmon') THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
     END);
+
+-- Insert the following data for vets.
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, '2000-04-23');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, '2019-01-17');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, '1981-05-04');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-06-08');    
+
     
