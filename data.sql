81b0ddbbc12d9b1571be7016087e2d1b76155618
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-09-22 22:53:04 -0400

    Change the previous info from data.sql to schema.sql

--- data.sql
+++ data.sql
@@ -17,26 +17,3 @@ VALUES
     ('Blossom', '1998-10-13', 3, true, 17),
     ('Ditto', '2022-05-14', 4, true, 22);
 
-CREATE TABLE owners (
-    id serial PRIMARY KEY,
-    full_name varchar(255),
-    age integer
-);
-
-CREATE TABLE species (
-    id serial PRIMARY KEY,
-    name varchar(255)
-);
-
--- id needs to be PRIMARY KEY and autoincr
-ALTER TABLE animals DROP CONSTRAINT animals_pkey;
-ALTER TABLE animals ADD PRIMARY KEY (id);
-
--- Drop species column
-ALTER TABLE animals DROP COLUMN species;
-
--- Add species_id as foreign key
-ALTER TABLE animals ADD COLUMN species_id integer REFERENCES species(id);
-
--- Add owner_id as foreign key
-ALTER TABLE animals ADD COLUMN owner_id integer REFERENCES owners(id);
