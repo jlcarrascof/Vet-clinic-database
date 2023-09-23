2cf217741f9a6ce9e06330241bc5c8a49ec82105
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-09-22 22:51:46 -0400

    Modify animals table

--- data.sql
+++ data.sql
@@ -27,3 +27,16 @@ CREATE TABLE species (
     id serial PRIMARY KEY,
     name varchar(255)
 );
+
+-- id needs to be PRIMARY KEY and autoincr
+ALTER TABLE animals DROP CONSTRAINT animals_pkey;
+ALTER TABLE animals ADD PRIMARY KEY (id);
+
+-- Drop species column
+ALTER TABLE animals DROP COLUMN species;
+
+-- Add species_id as foreign key
+ALTER TABLE animals ADD COLUMN species_id integer REFERENCES species(id);
+
+-- Add owner_id as foreign key
+ALTER TABLE animals ADD COLUMN owner_id integer REFERENCES owners(id);
